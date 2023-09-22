35a0bf51b9101843805aba54b923d87caaec17b6
isaiasrgomes <isaias.robson0@gmail.com>
2023-09-22 20:04:25 -0300

    add verifications in auth.php

--- auth.php
+++ auth.php
@@ -11,6 +11,24 @@ if (
 		$cpf = $_POST['cpf'];
 		$senha = $_POST['senha'];
 		$confirmaSenha = $_POST['confirmaSenha'];	
-		echo "Cadastro Realizado";
+		
+		if (strlen($nome) < 3){
+			echo "O nome precisa conter no minimo 3 caracteres <br>";
 	}
+		if(!strstr($email, '@')){
+			echo "@ não encontrado no campo Email <br>";
+		}
+		
+		if (strlen($cpf) < 11 || strlen($cpf) > 11){
+			echo "O CPF deve conter 11 digitos (somente números) <br>" ;
+		}
+		if(strlen($senha) < 6){
+			echo "A senha deve conter no mínimo 6 caracteres <br>";
+		}
+		
+		if($senha != $confirmaSenha){
+			echo "As senhas são diferentes <br>";
+		}
+		
+}
 ?>
