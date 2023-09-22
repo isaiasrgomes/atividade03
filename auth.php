28e61ddbedee826c04163e954060363543137fa0
isaiasrgomes <isaias.robson0@gmail.com>
2023-09-22 20:35:04 -0300

    finish conditions sucefully

--- auth.php
+++ auth.php
@@ -11,24 +11,34 @@ if (
 		$cpf = $_POST['cpf'];
 		$senha = $_POST['senha'];
 		$confirmaSenha = $_POST['confirmaSenha'];	
-		
+		$cad = true;
 		if (strlen($nome) < 3){
 			echo "O nome precisa conter no minimo 3 caracteres <br>";
+			$cad = false;
 	}
 		if(!strstr($email, '@')){
 			echo "@ não encontrado no campo Email <br>";
+			$cad = false;
 		}
 		
 		if (strlen($cpf) < 11 || strlen($cpf) > 11){
 			echo "O CPF deve conter 11 digitos (somente números) <br>" ;
+			$cad = false;
 		}
 		if(strlen($senha) < 6){
 			echo "A senha deve conter no mínimo 6 caracteres <br>";
+			$cad = false;
 		}
 		
 		if($senha != $confirmaSenha){
 			echo "As senhas são diferentes <br>";
+			$cad = false;
+			
 		}
-		
+		 if($cad){
+		 	echo "Cadastro realizado com sucesso!";
+		 } else {
+		 	echo "Erro ao cadastrar! Tente novamente";
+		 }
 }
 ?>
